348690578828240248c8e156909f2ae139d30536
Oscar Valenzuela <oscarvalenzuelab@users.noreply.github.com>
2016-01-26 22:52:26 -0800

    Update multidata.sh

--- multidata.sh
+++ multidata.sh
@@ -1,6 +1,6 @@
 #!/bi/bash
-KEY="56a68fe7c962eb0001aa012aab161bd8837b468955b01b03a6e847ab"
-PLATE="ABCD00"
+KEY="56a68fe7c962eb0001aa012aab161bd8837b468955b01b03a6e847ab" //Se otorga en el portal de desarrolladores
+PLATE="ABCD00" //Patente
 API="cars" #persons, cars, services
 METHOD="getplateinfo" #Ver catalogo de APIs
 
