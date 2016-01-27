6b3acf45891fb51df9e181c4f0ed0d83f1ff6cbd
Oscar Valenzuela <oscarvalenzuelab@users.noreply.github.com>
2016-01-26 22:45:37 -0800

    Create multidata.php

---
+++ multidata.php
@@ -0,0 +1,19 @@
+<?php
+$plate		= "ABCD00"; //Patente
+$key		= "56a68fe7c962eb000100012aab161bd8837b468955b01b03sde847ab"; //Se otorga en el portal de desarrolladores
+$api		= "cars"; //persons, cars, services
+$method		= "getplateinfo"; //Ver catalogo de APIs
+
+$headr		= array();
+$headr[]	= 'Content-length: 0';
+$headr[]	= 'Content-type: application/json';
+$headr[]	= 'Authorization: '.$key;
+$url		= "http://api.multidatachile.com/".$api."/".$method."/".$plate;
+
+$crl		= curl_init();
+curl_setopt($crl, CURLOPT_HTTPHEADER, $headr);
+curl_setopt($crl, CURLOPT_URL, $url);
+curl_setopt($crl, CURLOPT_POST, false);
+$rest		= curl_exec($crl);
+curl_close($crl);
+print_r($rest);
