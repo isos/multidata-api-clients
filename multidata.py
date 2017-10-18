480cd2fb4e6d79d46cdb91147a268eddf101b205
isos <desarrollo@isos.cl>
2017-10-18 13:22:07 -0300

    Update multidata.py

--- multidata.py
+++ multidata.py
@@ -1,9 +1,9 @@
 import requests
 
-dato="xl3525" #Patente a consultar.
+dato="11111111-1" #Rut a consultar.
 
 headers = {'Authorization': 'TU API KEY',} #Debe obtener la llave para acceder al api.
-url = requests.get('http://api.multidatachile.com/cars/jsongetowners/%s' % dato, headers=headers) #Mandamos el metodo y el api key con la url.
+url = requests.get('http://api.multidatachile.com/persons/jsongetowners/%s' % dato, headers=headers) #Mandamos el metodo y el api key con la url.
 respuesta=url.text # tomar la respuesta a nuestra peticion.
 
 data     = json.loads(respuesta) #trabajar la respuesta en JSON.
